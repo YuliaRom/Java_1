6366e1d26d30abf15399d0dd26cf1ac1e79fca53
Yulia <juliet-st0907@mail.ru>
2021-04-01 09:04:45 +0600

    Домашнее задание 2

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,62 +1,70 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+
+        //1
+        int a = 5;
+        int b = -10;
+        boolean c = sumBetween10and20(a,b);
+        System.out.println(c);
+
+        //2
+        positiveOrNegative(-8);
+
+        //3
+        boolean z = positiveOrNegative2(100);
+        System.out.println(z);
+
+        //4
+        textNTimes();
+
+        //5
+        boolean d = leapYear(2000);
+        System.out.println(d);
+    }
+
+    //Задание 1
+    public static boolean sumBetween10and20(int a, int b) {
+        int sum = (a + b);
+        if (sum > 10 && sum <= 20)
+            {return true;}
+        else
+            {return false;}
     }
 
     //Задание 2
-    public static void printThreeWords()
-    {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    public static void positiveOrNegative (int x) {
+        if (x >= 0)
+            System.out.println("Положительное число");
+        else
+            System.out.println("Отрицательное число");
     }
 
     //Задание 3
-
-    public static void checkSumSign() {
-        int a = 5;
-        int b = -50;
-        int sum = (a + b);
-
-        if (sum >= 0) {
-            System.out.println("Сумма положительная");
-        }
-
-        if (sum < 0) {
-            System.out.println("Сумма отрицательная");
-        }
+    public static boolean positiveOrNegative2(int y) {
+        if (y >= 0)
+        {return true;}
+        else
+        {return false;}
     }
 
     //Задание 4
-    public static void printColor() {
-        int value = 200;
-
-        if (value <= 0) {
-            System.out.println("Красный");
-        }
-
-        if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        }
-
-        if (value > 100) {
-            System.out.println("Зеленый");
+    public static void textNTimes () {
+        String text = ("Домашнее задание 2");
+        for (int n = 0; n < 5; n++) {
+            System.out.println(text);
         }
     }
 
     //Задание 5
-    public static void compareNumbers() {
-        int a = 500;
-        int b = 200;
+    public static boolean leapYear (int year) {
+        if (year % 4 == 0 && year % 100 !=0)
+        {return true;}
 
-        if (a>=b) {
-            System.out.println("a>=b");
-        }
+        else if (year % 400 == 0)
+        {return true;}
 
         else
-            System.out.println("a<b");
+        {return false;}
     }
 }
+
