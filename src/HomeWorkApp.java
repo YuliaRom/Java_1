6cd6bee6c12581c2b257662366adf84e246f9d78
Yulia <juliet-st0907@mail.ru>
2021-04-04 18:30:42 +0600

    Домашнее задание 3 (6, 7 задания)

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -14,6 +14,11 @@ public class HomeWorkApp {
         //5
         int[] result = someMethod(7, 1);
         System.out.println(Arrays.toString(result));
+        //6
+        minMax();
+        //7
+        int[] array = {2, 2, 2, 1, 2, 2, 10, 1};
+        System.out.println(balance(array));
 
     }
 
@@ -59,13 +64,12 @@ public class HomeWorkApp {
         int[][] array = new int[5][5];
         for (int i = 0; i < array.length; i++)
             for (int j = 0; j < array[i].length; j++) {
-                if ((i == j) || ((i + j)==(array.length-1)))
-                {
+                if ((i == j) || ((i + j) == (array.length - 1))) {
                     array[i][j] = 1;
                 }
             }
         for (int i = 0; i < array.length; i++)
-        System.out.println(Arrays.toString(array[i]));
+            System.out.println(Arrays.toString(array[i]));
 
     }
 
@@ -77,4 +81,49 @@ public class HomeWorkApp {
         }
         return array;
     }
+
+    //Задание 6
+    public static void minMax() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        {
+            int min = array[0];
+            int max = array[0];
+
+            for (int i = 1; i < array.length; i++) {
+                if (min > array[i]) {
+                    min = array[i];
+                }
+
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            }
+            System.out.println("Минимальное значение " + min);
+            System.out.println("Максимальное значение " + max);
+        }
+    }
+
+    //Задание 7
+    public static boolean balance(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int j = 0; j < i; j++) {
+                leftSum += array[j];
+            }
+
+            for (int n = i; n < array.length; n++) {
+                {
+                    rightSum += array[n];
+                }
+                if (leftSum == rightSum) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
