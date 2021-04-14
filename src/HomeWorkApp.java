bc46c7003d747c5b8492ae4c6545550ccebb8913
Yulia <juliet-st0907@mail.ru>
2021-04-14 19:59:05 +0600

    Домашнее задание 6

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,2 +1,20 @@
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        Cat Vaska = new Cat("Васька");
+        Dog Bobik = new Dog("Бобик");
+
+        Vaska.swim(50);
+        Bobik.swim(15);
+        Bobik.swim(10);
+        Vaska.run(300);
+        Vaska.run(150);
+        Bobik.run(600);
+        Bobik.run(400);
+
+        Cat Mashka = new Cat("Мурка");
+        Mashka.run(200);
+        Mashka.swim(20);
+
+        System.out.println("Мы создали " + Animal.animalCount + " животных!");
+    }
 }
